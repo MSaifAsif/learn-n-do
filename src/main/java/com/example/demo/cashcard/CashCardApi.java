50c29599c0f7640a97698a1644b278a9822d033d
MSaifAsif <saifasifmirza@gmail.com>
2024-05-11 17:28:32 +0200

    Tessting out name

--- src/main/java/com/example/demo/cashcard/CashCardApi.java
+++ src/main/java/com/example/demo/cashcard/CashCardApi.java
@@ -2,9 +2,7 @@ package com.example.demo.cashcard;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
