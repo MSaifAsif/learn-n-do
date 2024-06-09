7e8fadeb6661fdd2b3f7386db62ee4a7c924718c
MSaifAsif <saifasifmirza@gmail.com>
2024-06-09 18:41:18 +0200

    First implementation of graphQL

---
+++ src/test/java/com/example/demo/CashCardApplicationTestsGraphQl.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.graphql.test.tester.GraphQlTester;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class CashCardApplicationTestsGraphQl {
+
+    @Autowired
+    private GraphQlTester graphQlTester;
+
+    @Test
+    void shouldGetFirstBook() {
+        this.graphQlTester
+                .documentName("bookDetails")
+                .variable("id", "book-1")
+                .execute()
+                .path("bookById")
+                .matchesJson("""
+                    {
+                        "id": "book-1",
+                        "name": "Effective Java",
+                        "pageCount": 416,
+                        "author": {
+                          "firstName": "Joshua",
+                          "lastName": "Bloch"
+                        }
+                    }
+                """);
+    }
+}
